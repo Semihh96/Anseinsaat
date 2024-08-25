de5ff448303bfd0dd5069cbc93aecf30b83092f2
Semihh96 <semihgunbay1907@gmail.com>
2024-08-25 20:00:02 +0300

    versiyon1

---
+++ anseinsaat/foooter.php
@@ -0,0 +1,192 @@
+<div class="rts-footer-area footer-one footer-bg bg_image">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="footer-top-wrapper  ptb--80">
+                    <a href="#" class="logo-area" data-sal="zoom-in" data-sal-delay="150" data-sal-duration="800">
+                        <img src="assets/images/logo/anse-logo-beyaz.png" alt="blog-images">
+                    </a>
+                    <h4 class="title" data-sal="zoom-in" data-sal-delay="150" data-sal-duration="800">
+                        Anse Mimarlık İnşaat
+                    </h4>
+                    <div class="subscribe-area" data-sal="zoom-in" data-sal-delay="150" data-sal-duration="800">
+                        <form>
+                            <input type="email" name="email" placeholder="E-Posta" required="">
+                            <button class="rts-btn btn-primary">İletişim</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="main-footer-one-area ptb--80">
+                    <div class="single-footer-inner" data-sal="slide-up" data-sal-delay="150" data-sal-duration="800">
+                        <div class="body one">
+                            <h6 class="footer-title">
+                                Hakkımızda
+                            </h6>
+                            <p class="disc">
+                                Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
+                                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                            </p>
+                            <div class="social-wrapper-one">
+                                <ul>
+                                    <li>
+                                        <a href="#">
+                                            <i class="fa-brands fa-facebook-f"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <i class="fa-brands fa-twitter"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <i class="fa-brands fa-youtube"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <i class="fa-brands fa-linkedin-in"></i>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="single-footer-inner" data-sal="slide-up" data-sal-delay="350" data-sal-duration="800">
+                        <div class="body">
+                            <h6 class="footer-title">
+                                Hızlı Link
+                            </h6>
+                            <div class="use-full-link-wrapper">
+                                <a href="index.php" class="single">
+                                    <i class="fa-sharp fa-solid fa-right"></i>
+                                    Anasayfa
+                                </a>
+                                <a href="#" class="single">
+                                    <i class="fa-sharp fa-solid fa-right"></i>
+                                    Hakkımızda
+                                </a>
+                                <a href="#" class="single">
+                                    <i class="fa-sharp fa-solid fa-right"></i>
+                                    Hizmetlerimiz
+                                </a>
+                                <a href="#" class="single">
+                                    <i class="fa-sharp fa-solid fa-right"></i>
+                                    Projeler
+                                </a>
+                                <a href="#" class="single">
+                                    <i class="fa-sharp fa-solid fa-right"></i>
+                                    Medya
+                                </a>
+                                <a href="#" class="single">
+                                    <i class="fa-sharp fa-solid fa-right"></i>
+                                    İletişim
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="single-footer-inner" data-sal="slide-up" data-sal-delay="550" data-sal-duration="800">
+                        <div class="body">
+                            <h6 class="footer-title">
+                                İletişim
+                            </h6>
+                            <div class="contactus-link-wrapper">
+                                <div class="single">
+                                    <div class="icon">
+                                        <i class="fa-sharp fa-solid fa-phone"></i>
+                                    </div>
+                                    <a href="#" class="number">
+                                        +110 111 00 10
+                                    </a>
+                                </div>
+                                <div class="single">
+                                    <div class="icon">
+                                        <i class="fa-sharp fa-solid fa-phone"></i>
+                                    </div>
+                                    <a href="#" class="number">
+                                        +100 011 11 00
+                                    </a>
+                                </div>
+                                <div class="single">
+                                    <div class="icon">
+                                        <i class="fa-sharp fa-regular fa-envelope"></i>
+                                    </div>
+                                    <a href="#" class="number">
+                                        info@anse.com.tr
+                                    </a>
+                                </div>
+                                <div class="single">
+                                    <div class="icon">
+                                        <i class="fa-light fa-location-dot"></i>
+                                    </div>
+                                    <a href="#" class="number mt-dec">
+                                        Çankaya, Ankara
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="single-footer-inner" data-sal="slide-up" data-sal-delay="750" data-sal-duration="800">
+                        <div class="body">
+                            <h6 class="footer-title">
+                                Medya
+                            </h6>
+                            <div class="popular-post-wrapper">
+                                <div class="single-popular-post">
+                                    <a href="#" class="thumbnail">
+                                        <img src="assets/images/footer/01.png" alt="popular-post">
+                                    </a>
+                                    <div class="details-area">
+                                        <div class="logo">
+                                            <i class="fa-regular fa-clock"></i>
+                                            15 Mayıs, 2022
+                                        </div>
+                                        <a href="#" class="tilte-area">
+                                            <h6 class="title">Lorem ipsum dolor</h6>
+                                        </a>
+                                        <a href="#" class="btn-read-more">Devamını Oku <i class="fa-solid fa-arrow-right"></i></a>
+                                    </div>
+                                </div>
+                                <div class="single-popular-post">
+                                    <a href="#" class="thumbnail">
+                                        <img src="assets/images/footer/02.png" alt="popular-post">
+                                    </a>
+                                    <div class="details-area">
+                                        <div class="logo">
+                                            <i class="fa-regular fa-clock"></i>
+                                            15th Mayıs, 2022
+                                        </div>
+                                        <a href="#" class="tilte-area">
+                                            <h6 class="title">Lorem ipsum dolor</h6>
+                                        </a>
+                                        <a href="#" class="btn-read-more">Read More <i class="fa-solid fa-arrow-right"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="copyright-area-one">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="wrapepr">
+                        <p>Copyright 2023 Anse Mimarlık İnşaat</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="progress-wrap">
+    <svg class="progress-circle svg-content" width="100%" height="100%" viewbox="-1 -1 102 102">
+        <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" style="transition: stroke-dashoffset 10ms linear 0s; stroke-dasharray: 307.919, 307.919; stroke-dashoffset: 307.919;"></path>
+    </svg>
+</div>
---
+++ anseinsaat/header.php
@@ -0,0 +1,116 @@
+<header class="header-one">
+    <div class="header-top">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="header-top-wrapper">
+                        <div class="left">
+                            <div class="icon">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="13" height="17" viewbox="0 0 13 17" fill="none">
+                                    <path d="M6.95455 6.95454H12.3636L5.40909 17V10.0455H0L6.95455 -3.8147e-06V6.95454Z" fill="#be2e21"></path>
+                                </svg>
+                            </div>
+                            <p class="disc">
+                              Anse Mimarlık İnşaat
+                            </p>
+                        </div>
+                        <div class="right">
+                            <ul>
+                                <li class="single"><a href="#">Kariyer</a></li>
+                                <li class="single last"><a href="#">İletişim</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="header-mid">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="header-mid-one-wrapper">
+                        <div class="logo-area">
+                            <a href="index.php">
+                                <img src="assets/images/logo/anse-logo-2.png" alt="logo">
+                            </a>
+                        </div>
+                        <div class="header-right">
+                            <div class="single-component info-con">
+                                <div class="icon">
+                                    <i class="fal fa-map-marker-alt"></i>
+                                </div>
+                                <div class="info">
+                                    <span>Adres</span>
+                                    <a href="#">Çankaya, Ankara</a>
+                                </div>
+                            </div>
+                            <div class="single-component info-con">
+                                <div class="icon">
+                                    <i class="fal fa-envelope"></i>
+                                </div>
+                                <div class="info">
+                                    <span>E-Posta</span>
+                                    <a href="#">info@anse.com.tr</a>
+                                </div>
+                            </div>
+                            <div class="single-component">
+                                <a href="#" class="rts-btn btn-primary">Bize Ulaşın
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="13" viewbox="0 0 14 13" fill="none">
+                                        <path d="M11.0039 3.414L2.39691 12.021L0.98291 10.607L9.58891 2H2.00391V-3.8147e-06H13.0039V11H11.0039V3.414Z" fill="white"></path>
+                                    </svg>
+                                </a>
+                                <div class="menu-area d-block d-lg-none" id="menu-btn">
+                                    <div class="icon">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="16" viewbox="0 0 18 16" fill="none">
+                                            <rect width="18" height="2" fill="#0C0A0A"></rect>
+                                            <rect y="7" width="18" height="2" fill="#0C0A0A"></rect>
+                                            <rect y="14" width="18" height="2" fill="#0C0A0A"></rect>
+                                        </svg>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="nav-area-one  header--sticky">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="nav-header-area">
+                        <div class="clip-path"></div>
+                        <div class="wrapper">
+                            <div class="main-nav-desk nav-area">
+                                <nav>
+                                    <ul>
+                                        <li class="menu-item">
+                                            <a class="nav-item" href="index.php">Anasayfa</a>
+                                        </li>
+                                        <li class="menu-item">
+                                            <a class="nav-item" href="#">Hakkımızda</a>
+                                        </li>
+                                        <li class="menu-item">
+                                            <a class="nav-item" href="#">Hizmetlerimiz</a>
+                                        </li>
+                                        <li class="menu-item">
+                                            <a class="nav-item" href="#">Projeler</a>
+                                        </li>
+                                        <li class="menu-item">
+                                            <a class="nav-item" href="#">Medya</a>
+                                        </li>
+                                        <li class="menu-item">
+                                            <a class="nav-item" href="#">İletişim</a>
+                                        </li>
+                                    </ul>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
